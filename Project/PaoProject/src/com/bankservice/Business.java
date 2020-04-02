4065aff80e070303134340e5f8422ca0d6119ccf
andrei828 <andrei.liviu10@gmail.com>
2020-04-02 17:22:39 +0300

    removed first project

--- Project/PaoProject/src/com/bankservice/Business.java
+++
@@ -1,7 +0,0 @@
-package com.bankservice;
-
-public class Business extends Client {
-    public Business(String name, String ...address) {
-        super(name, address);
-    }
-}
--- Project/PaoProject/src/com/bankservice/Person.java
+++
@@ -1,7 +0,0 @@
-package com.bankservice;
-
-public class Person extends Client {
-    public Person(String name, String ...address) {
-        super(name, address);
-    }
-}
